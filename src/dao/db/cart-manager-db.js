a56408b619be5e4ec58ac4da558a90aae2ad0bc7
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-07-16 11:57:22 -0300

    cart-manager-dv ok

---
+++ src/dao/db/cart-manager-db.js
@@ -0,0 +1,59 @@
+import CartModel from "../models/cart.model.js";
+
+class CartManager {
+
+
+
+    async crearCarrito() {
+        try {
+            const nuevoCarrito = new CartModel({products:[]})
+            await nuevoCarrito.save()
+            return nuevoCarrito
+        } catch (error) {
+            console.log("Error al crear carrito", error);
+            throw error;
+        }
+    }
+
+    async getCarritoById(cartId) {
+        try {
+            const carrito = await CartModel.findById(cartId)
+            if (!carrito) {
+                throw new Error(`No existe un carrito con el id ${cartId}`)
+            }
+
+            if (!carrito) {
+                throw new Error(`No existe un carrito con el id ${cartId}`);
+            }
+
+            return carrito;
+
+        } catch (error) {
+            console.error("Error al obtener el carrito por ID", error);
+            throw error;
+        }
+    }
+
+    async agregarProductoAlCarrito(cartId, productId, quantity = 1) {
+        try {
+            const carrito = await this.getCarritoById(cartId)
+            const existeProducto = carrito.products.find( item => item.product.toString() === productId)
+
+            if(existeProducto) {
+                existeProducto.quantity += quantity
+            }else {
+                carrito.products.push({product: productId, quantity})
+            }
+
+            carrito.markModified("products")
+            await carrito.save()
+            return carrito
+
+        } catch (error) {
+            console.error("Error al agregar producto", error);
+            throw error;
+        }
+    }
+}
+
+export default CartManager;
